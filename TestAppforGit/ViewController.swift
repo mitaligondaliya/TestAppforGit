53461231e3d12728884037cbc5122836e62d8c49
Mitali Gondaliya <gondaliyamitali19@gmail.com>
2019-04-09 20:42:46 +0530

    Added new person

--- TestAppforGit/ViewController.swift
+++ TestAppforGit/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var person = "Mitali Gondaliya"
+    
+    //MARK: - LIFECYCLE
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
